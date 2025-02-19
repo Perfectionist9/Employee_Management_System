de10387e58264840da6f0b4d41198a3b37e9a74a
Perfectionist9 <premvrma07@gmail.com>
2025-02-19 19:09:42 +0530

    first commit

--- src/Components/sub_Emp/Navbar.jsx
+++ src/Components/sub_Emp/Navbar.jsx
@@ -2,16 +2,10 @@ import React, { useState } from 'react'
 
 const Navbar = ({data}) => {
 
- const [user, setUser] = useState('')
-
- if(!data){
-  setUser('Admin')
- }
- else user(data.firstName)
 
   return (
     <div className='flex items-center justify-between ' >
-        <h1 className='text-2xl font-medium'>HELLO <br /> <span className='text-3xl font-semibold'> {user} 🙋‍♂️ </span></h1>
+        <h1 className='text-2xl font-medium'>HELLO <br /> <span className='text-3xl font-semibold'> {data.firstName} 🙋‍♂️ </span></h1>
         <button className='bg-red-500 rounded py-2 px-3 text-lg font-medium '>Log out</button>
     </div>
   )
